5316b0cc681c9acd00972d482436cc083cf39747
clonetech <kencheruiyot.kc@gmail.com>
2018-07-05 21:49:05 +0300

    maket it better

--- jackpot/views.py
+++ jackpot/views.py
@@ -65,12 +65,12 @@ def hexabet(request):
 
     args = {}
 
-    hexabet_teams = Hexabet.objects.filter(
+    hexabet = Hexabet.objects.filter(
         published_date__lte=timezone.now()
     ).order_by('-published_date')[:6]
 
 
-    args ['hexabet_teams'] = hexabet_teams
+    args ['hexabet'] = hexabet
 
     return render(request, 'hexabet.html', args)
 
