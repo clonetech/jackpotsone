eeb066d6eeaf7455d750b81436eb373972ec8f9d
clonetech <kencheruiyot.kc@gmail.com>
2018-07-05 17:32:19 +0300

    make it better

--- jackpot/urls.py
+++ jackpot/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 from .views import home
 from . import views
 from . import views as core_views
+from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 
 app_name = 'jackpot'
@@ -18,5 +20,7 @@ urlpatterns = [
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
     path('signup/', core_views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
